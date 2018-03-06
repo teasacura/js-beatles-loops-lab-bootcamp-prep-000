6755422fde4becfe4d545d9ff7d71dbe6344b758
Eva Leake <teasacuraster@gmail.com>
2018-03-06 17:41:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(arrayMusicians, arrayInstruments) {
   var emptyArray = [];
   for (var i = 0; i < 4; i++) {
-  let newString = `$(arrayMusicians[i]) plays $(arrayInstruments[i])`
+  let newString = "arrayMusicians[i]" + "plays " + " arrayInstruments[i]"
   emptyArray.push(newString)
   }
   return emptyArray
